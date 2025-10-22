67d6fd25561fc7b88935f77c6a3e0345b56b9cf3
Clubpiren <130805081+pirenclub@users.noreply.github.com>
2025-10-22 11:44:05 -0300

    Delete js directory

--- js/config.example.js
+++
@@ -1,8 +0,0 @@
-// If both files exist (config.js and config.example.js), the site uses config.js.
-// Fill your values here.
-window.CFG = {
-  // Google Apps Script Web App endpoint (from Deploy > Manage deployments)
-  ENDPOINT: "PASTE_YOUR_APPS_SCRIPT_WEBAPP_URL_HERE",
-  // Your WhatsApp number in international format, e.g., "54911XXXXXXX"
-  WHATSAPP_PHONE: "549XXXXXXXXX"
-};
--- js/config.js
+++
@@ -1,7 +0,0 @@
-// Fill your values here.
-window.CFG = {
-  // Google Apps Script Web App endpoint (from Deploy > Manage deployments)
-  ENDPOINT: "PASTE_YOUR_APPS_SCRIPT_WEBAPP_URL_HERE",
-  // Your WhatsApp number in international format, e.g., "54911XXXXXXX"
-  WHATSAPP_PHONE: "549XXXXXXXXX"
-};
